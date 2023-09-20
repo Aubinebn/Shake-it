99f93ddf233b342b4138db6d14ba59e215d4eea3
Aubin EBANO <ebano.aubin@gmail.com>
2023-09-20 18:46:21 +0200

    Initial Commit

---
+++ Shake it/ShakeitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shake_itApp.swift
+//  Shake it
+//
+//  Created by Aubin Ebano on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShakeitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
